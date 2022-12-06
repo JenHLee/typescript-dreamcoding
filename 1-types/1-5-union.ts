49e30bf156ae3ceb414a20131a7670800f3ac04d
JenHLee <hj.jennie.lee@gmail.com>
2022-12-06 01:46:06 -0700

    added discriminated union of typescript

--- 1-types/1-5-union.ts
+++ 1-types/1-5-union.ts
@@ -10,36 +10,4 @@
 
     type TileSize = 8 | 16 | 32;
     const tile: TileSize = 16;
-
-    // function : login -> success, fail
-    type SuccessState = {
-        response: {
-            body: string;
-        };
-    }
-    type FailState = {
-        reason: string;
-    }
-
-    type LoginState = SuccessState | FailState;
-    function login(): LoginState {
-        return {
-            response: {
-                body: 'logged in!',
-            },
-        };
-    }
-
-    // printLoginState(state)
-    // success -> 🎉 body
-    // fail -> 😢 reason
-
-    function printLoginState(state: LoginState) {
-        // function printLoginState(state: LoginState): void {  // void can be removed
-        if ('response' in state) {
-            console.log(`🎉 ${state.response.body}`);
-        } else {
-            console.log(`😢 ${state.reason}`);
-        }
-    }
 }
---
+++ 1-types/1-6-discriminated.ts
@@ -0,0 +1,36 @@
+{
+    // function : login -> success, fail
+    type SuccessState = {
+        result: 'success';
+        response: {
+            body: string;
+        };
+    }
+    type FailState = {
+        result: 'fail';
+        reason: string;
+    }
+
+    type LoginState = SuccessState | FailState;
+    function login(): LoginState {
+        return {
+            result: 'success',
+            response: {
+                body: 'logged in!',
+            },
+        };
+    }
+
+    // printLoginState(state)
+    // success -> 🎉 body
+    // fail -> 😢 reason
+
+    function printLoginState(state: LoginState) {
+        // function printLoginState(state: LoginState): void {  // void can be removed
+        if (state.result === 'success') {
+            console.log(`🎉 ${state.response.body}`);
+        } else {
+            console.log(`😢 ${state.reason}`);
+        }
+    }
+}

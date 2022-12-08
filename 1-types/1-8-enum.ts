e1cf4f286983fcc17c6f49411c7b04e87f9e607d
JenHLee <hj.jennie.lee@gmail.com>
2022-12-08 00:17:25 -0700

    added enum.ts, compare to union

--- 1-types/1-8-enum.ts
+++ 1-types/1-8-enum.ts
@@ -8,4 +8,51 @@
     const MONDAY = 0;
     const TUESDAY = 1;
     const WEDNESDAY = 2;
+    const DAYS_ENUM = Object.freeze({"MONDAY": 0, "TUESDAY":1, "WEDNESDAY": 2})
+    const dayOfToday = DAYS_ENUM.MONDAY;
+
+    // TypeScript
+    enum Days{
+        Monday, // 0
+        Tuesday, // 1
+        Wednesday, // 2
+        Thursday, // 3
+        Friday, // 4
+        Saturday, // 5
+        Sunday, // 6
+    }
+
+    // if set the number, then automatically increased by order
+    enum SetDays{
+        Monday = 1,
+        Tuesday, // 1
+        Wednesday, // 2
+        Thursday, // 3
+        Friday, // 4
+        Saturday, // 5
+        Sunday, // 6
+    }
+
+        // if set the string, then should be set all manullay
+        enum SetLetterDays{
+            Monday = 'mon',
+            Tuesday = 'tue',
+            Wednesday = 'wed',
+            Thursday = 'thu',
+            Friday = 'fri',
+            Saturday = 'sat',
+            Sunday = 'sun'
+        }
+    console.log(Days.Monday);
+    console.log(Days.Tuesday);
+    console.log("--------------")
+    console.log(SetDays.Monday);
+    console.log(SetDays.Tuesday);
+    console.log("--------------")
+    console.log(SetLetterDays.Monday);
+    console.log(SetLetterDays.Tuesday);
+    console.log("--------------")
+    const day = Days.Saturday;
+    console.log(day);
+
 }

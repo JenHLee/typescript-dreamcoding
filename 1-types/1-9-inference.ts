f42638ef7a557d3c9d30088e0e8ca84345273dbc
JenHLee <hj.jennie.lee@gmail.com>
2022-12-08 00:26:23 -0700

    created inference.ts that inference of typescript

---
+++ 1-types/1-9-inference.ts
@@ -0,0 +1,19 @@
+/**
+ * Type Inference
+ */
+
+let text = 'hello';
+function print (message = 'hello'){ // if I put default parameter as a string type, then I can only use string type for message
+    console.log(message);
+}
+
+print('hello');
+// print(1); // that's why I can't send number to message
+
+function add(x: number, y: number){
+    return x + y;
+}
+
+const result = add (1, 2); // no need to put the type beside result, because it already assigned in add. result should be number
+// const result1 = add (a, b); // I can only send number, string can't be sent
+

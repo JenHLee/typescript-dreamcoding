ed64f62fa08644be27ee6882fc55dbb5c18019eb
JenHLee <hj.jennie.lee@gmail.com>
2022-12-07 23:59:08 -0700

    created enum.ts

---
+++ 1-types/1-8-enum.ts
@@ -0,0 +1,11 @@
+{
+    /**
+     * Enum
+     */
+    // JavaScript
+    const MAX_NUM = 6;
+    const MAX_STUDENTS_PER_CLASS = 10;
+    const MONDAY = 0;
+    const TUESDAY = 1;
+    const WEDNESDAY = 2;
+}

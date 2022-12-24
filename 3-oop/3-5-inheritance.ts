45b34ca7e89f310b7d7ae07474286b7567649436
JenHLee <hj.jennie.lee@gmail.com>
2022-12-23 23:21:09 -0700

    added console.log machine in inheritance.ts

--- 3-oop/3-5-inheritance.ts
+++ 3-oop/3-5-inheritance.ts
@@ -82,5 +82,6 @@
     const latteMachine = new CaffeLatteMachine(23, 'SSSS');
     const coffee = latteMachine.makeCoffee(1);
     console.log(coffee);
+    console.log(machine);
     console.log('Serial Number: ' + latteMachine.serialNumber);
 }

200c75a3a438d9163549681f9c8c2238bdffd991
JenHLee <hj.jennie.lee@gmail.com>
2023-01-04 23:11:40 -0700

    created constrains with generic

---
+++ 5-generic/5-3-constrains.ts
@@ -0,0 +1,46 @@
+interface Employee {
+    pay(): void;
+
+}
+
+class FullTimeEmployee implements Employee {
+    pay(): void {
+        console.log(`full time!!`);
+
+    }
+    workFullTime() {
+
+    }
+}
+
+class PartTimeEmployee implements Employee {
+    pay() {
+        console.log(`part time!!`);
+
+    }
+    workPartTime() {
+
+    }
+}
+
+// 세부적인 타입을 인자로 받아서 정말 추상적인 타입으로 다시 리턴하는 함수는 💩💩💩
+function payBad(employee: Employee): Employee {
+    employee.pay();
+    return employee;
+}
+
+function pay<T extends Employee>(employee: T) : T {
+    employee.pay();
+    return employee;
+}
+
+const jennie = new FullTimeEmployee();
+const bob = new PartTimeEmployee();
+jennie.workFullTime();
+bob.workPartTime();
+
+// payBad를 이용하면 employee가 fulltime인지 partime인지 정보를 잃어버림 
+// as를 이용하여 그 값을 다시 저장함, 하지만 이는 좋지 못한 방법 💩💩💩
+// 이를 예방하기위해 pay함수처럼 generic을 이용함 ✨✨✨
+const jennieAfterPay = payBad(jennie) as FullTimeEmployee; 
+const bobAfterPay = payBad(bob) as PartTimeEmployee;

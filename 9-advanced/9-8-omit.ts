824352fee7438f3dcd1b5268da81641f2b46bac0
JenHLee <hj.jennie.lee@gmail.com>
2023-01-11 16:42:26 -0700

    created omit of type

---
+++ 9-advanced/9-8-omit.ts
@@ -0,0 +1,28 @@
+{
+    // Pick의 반대, Omit은 선택한 것을 뺀 나머지 값
+   type Video = {
+        id: string;
+        title: string;
+        url: string;
+        data: string;
+    };
+
+    type VideoMetaData = Omit<Video, 'url' | 'data'>;
+
+    function getVideo(id: string): Video {
+        return {
+            id,
+            title: 'video',
+            url: 'http://..',
+            data: 'byte-data...'
+        }
+    }
+
+    function getVideoMetaData(id: string): VideoMetaData {
+        return {
+            id: id,
+            title: 'title',
+            // url: 'url'
+        }
+    }
+}
